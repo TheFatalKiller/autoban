c1cac06391a3d9d49e7f243b6e9d045899e2be58
TheFatalKiller <47860911+TheFatalKiller@users.noreply.github.com>
2021-04-04 21:20:38 +0100

    Update autoban.py

--- autoban/autoban.py
+++ autoban/autoban.py
@@ -10,7 +10,7 @@ class AutoBan(commands.Cog):
     async def on_member_join(self, member):
         guild = member.guild
         if member.id in self.bans:
-            embed = discord.Embed(title="AutoBanned", description=f"You have been AutoBanned in **{guild.name}**, DM **Fatal#0007** to appeal your removal from banlist.", color=self.bot.main_color)
+            embed = discord.Embed(title="AutoBanned", description="You have been AutoBanned in **{guild.name}**, DM **Fatal#0007** to appeal your removal from banlist.", color=self.bot.main_color)
             await member.send(embed=embed)
             await guild.ban(member, reason="AutoBanned by being placed on the AutoBan List by Fatal")
 
