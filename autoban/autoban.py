b22ed64f4c02cc6f74ea184e0050d7059342714d
TheFatalKiller <47860911+TheFatalKiller@users.noreply.github.com>
2021-05-08 20:06:11 +0100

    Update autoban.py

--- autoban/autoban.py
+++ autoban/autoban.py
@@ -4,14 +4,14 @@ from discord.ext import commands
 class AutoBan(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bans = [197137836518932480, 613249974163210261, 613137906646646807, 399733547281285121, 716312920362975252, 491610326496837642, 755547592842149950, 531602381591543818, 543286363156643903, 333808567268147211, 210049870294286337, 812533315222765595, 459476688443801610, 718119648754663475]
+        self.bans = [804861326244708373, 197137836518932480, 613249974163210261, 613137906646646807, 399733547281285121, 716312920362975252, 491610326496837642, 755547592842149950, 531602381591543818, 543286363156643903, 333808567268147211, 210049870294286337, 812533315222765595, 459476688443801610, 718119648754663475]
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
         channel = self.bot.get_channel(828360183280959498)
         if member.id in self.bans:
-            embed = discord.Embed(title="AutoBanned", description=f"You have been AutoBanned in **{guild.name}**, DM **Fatal#0007** to appeal your removal from banlist, or appeal your ban by visiting this forum: https://forms.gle/ty29zXraHfxHQpkx6", color=self.bot.main_color)
+            embed = discord.Embed(title="AutoBanned", description=f"You have been AutoBanned in **{guild.name}**, DM **Fatal#7702** to appeal your removal from banlist, or appeal your ban by visiting this forum: https://forms.gle/ty29zXraHfxHQpkx6", color=self.bot.main_color)
             em = discord.Embed(title="Autoban Logs", description=f"{member} was autobanned")
             await member.send(embed=embed)
             await channel.send("<@199622722660204545>")
